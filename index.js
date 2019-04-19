c2d800ff19a3a649b1f4036330526d0663ac75d9
achyutb6 <achyut.b6@gmail.com>
2019-04-18 19:23:32 -0500

    connection handling

--- index.js
+++ index.js
@@ -19,13 +19,37 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 var router = express.Router();
-var connection = mysql.createPool({
+var db_config =  {
     host: 'us-cdbr-iron-east-02.cleardb.net',
     user: 'b6f017c0866ed2',
     password: '3c07d231',
     database: 'heroku_b9ad136c5d2fbf3',
-});
-connection.connect();
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 app.use(function(request, result, next) {
     result.header("Access-Control-Allow-Origin", "*");
